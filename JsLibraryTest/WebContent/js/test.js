837ec719fe55936823e8f32395ad9f28c63962e4
ishino <ishino.y@opst.co.jp>
2016-05-18 16:24:36 +0900

    new

---
+++ JsLibraryTest/WebContent/js/test.js
@@ -0,0 +1,11 @@
+/**
+ *
+ */
+
+$(function() {
+	$('#button').click(function() {
+		location.href = 'http://google.com'
+//		var name = $('#name').val();
+//		alert('こんにちは、' + name + "さん");
+	});
+});
